8ebf6b015ddb914437f773bd9754711fadf908d1
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2025-03-18 02:11:58 -0300

    initialize project word search "nega layla".
    starting server fastify, config zod validate date, config swagger in doc

---
+++ src/server.ts
@@ -0,0 +1,48 @@
+import { fastify } from "fastify";
+import {
+  validatorCompiler,
+  serializerCompiler,
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
+import { z } from "zod";
+import { env } from "./env";
+import { helloRoute } from "./routes/hello-route-api";
+
+const app = fastify({ logger: true }).withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+// Temporary when production will be changed..
+app.register(fastifyCors, {
+  origin: true,
+});
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Word Search API",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+app.register(helloRoute)
+
+app
+  .listen({ port: env.PORT })
+  .then(() => {
+    console.log(`Server running on port ${env.PORT}`);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
