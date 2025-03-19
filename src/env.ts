266af2b7b49b04970565e463d26ee390ab1c7094
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2025-03-19 18:19:13 -0300

    add DB_FILE_NAME to environment schema

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,8 @@ import { z } from 'zod'
 
 export const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
-  FIREBASE_ADMIN: z.string()
+  FIREBASE_ADMIN: z.string(),
+  DB_FILE_NAME: z.string()
 })
 
 export const env = envSchema.parse(process.env)
