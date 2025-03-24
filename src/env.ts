c94f4283998640d0cc0f45f87903f5682189199f
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2025-03-24 01:37:20 -0300

    feat: add URL_API_LLAMA to environment schema

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,8 @@ import { z } from 'zod'
 export const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   FIREBASE_ADMIN: z.string(),
-  DB_FILE_NAME: z.string()
+  DB_FILE_NAME: z.string(),
+  URL_API_LLAMA: z.string()
 })
 
 export const env = envSchema.parse(process.env)
