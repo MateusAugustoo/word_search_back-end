06517fb1433509a6700470df442d45563c5142ee
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2025-03-25 00:46:27 -0300

    feat: rename URL_API_LLAMA to API_URL_LLAMA in environment schema

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,7 @@ export const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   FIREBASE_ADMIN: z.string(),
   DB_FILE_NAME: z.string(),
-  URL_API_LLAMA: z.string()
+  API_URL_LLAMA: z.string()
 })
 
 export const env = envSchema.parse(process.env)
