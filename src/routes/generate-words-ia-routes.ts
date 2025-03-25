ee0ee3c751ac6937bb87036557bfc0ec16020e9a
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2025-03-25 00:46:47 -0300

    feat: update words-search-ia route to include language parameter and enhance response schema

--- src/routes/generate-words-ia-routes.ts
+++ src/routes/generate-words-ia-routes.ts
@@ -3,17 +3,46 @@ import { generateWords } from "../functions/generate-words-ia";
 import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 
 export const generateWordsRoutes: FastifyPluginAsyncZod = async (app) => {
-  app.post('/generate/words-search-ia', {
-    schema: {
-      tags: ['generate-words'],
-      description: `Generate words by search IA`,
-      body: z.object({
-        length: z.number(),
-        theme: z.string(),
-      })
+  app.post(
+    "/generate/words-search-ia",
+    {
+      schema: {
+        tags: ["generate-words"],
+        description: `Generate words by search IA`,
+        body: z.object({
+          language: z.string(),
+          length: z.number(),
+          theme: z.string(),
+        }),
+        response: {
+          200: z.object({
+            message: z.string(),
+            words: z.array(z.string()),
+          }),
+          400: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { length, theme, language } = request.body;
+
+      const { words } = await generateWords({ length, theme, language });
+
+      if (!words) {
+        reply.code(400).send({
+          message: "Error to generate words",
+        });
+        return;
+      }
+
+      const response = {
+        message: "Words generated successfully",
+        words,
+      };
+
+      reply.code(200).send(response);
     }
-  }, async (request, reply) => {
-    const { length, theme } = request.body
-    
-  });
-}
+  );
+};
