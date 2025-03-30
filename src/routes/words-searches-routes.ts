94e3c62eb20e879d041164e5e86e77ab3da3febd
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2025-03-30 14:59:01 -0300

    feat(wordsSearch): add GET endpoint for retrieving word searches by user ID
    Implement a new GET endpoint `/wordsSearch` that retrieves word searches for a specific user. The endpoint uses `authMiddleware` for authentication and returns a list of searches along with the total count. This feature enhances the application by allowing users to access their search history.

--- src/routes/words-searches-routes.ts
+++ src/routes/words-searches-routes.ts
@@ -1,4 +1,42 @@
 import { z } from "zod";
+import { wordsSearchFunctions } from "../functions/words-search-functions";
 import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { authMiddleware } from "../middlewares/authTokenUser";
 
-export const wordsSearchRoutes: FastifyPluginAsyncZod = async (app) => {}
+export const wordsSearchRoutes: FastifyPluginAsyncZod = async (app) => {
+  app.get(
+    "/wordsSearch",
+    {
+      preHandler: [authMiddleware],
+      schema: {
+        tags: ["wordsSearch"],
+        description: "Words Search",
+        querystring: z.object({
+          userId: z.string(),
+        }),
+        response: {
+          200: z.object({
+            response: z.object({
+              searches: z.array(z.any()),
+              total: z.number(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const userId = request.query.userId;
+
+      const { searches, total } = await wordsSearchFunctions.getAllByUserId(
+        userId
+      );
+
+      const response = {
+        searches,
+        total,
+      };
+
+      return reply.code(200).send({ response });
+    }
+  );
+};
